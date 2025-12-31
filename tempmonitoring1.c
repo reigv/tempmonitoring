59daa71d85fd8012ce62ffe4c232f8a6dc17ba64
reigv <reig.vaeo.soiv099@gmail.com>
2025-12-31 03:43:54 +0000

    1.1

--- tempmonitoring1.c
+++ tempmonitoring1.c
@@ -2,4 +2,14 @@
 
 int main() {
     printf("Temp monitor");
+
+    int valid_entry = 0; //flag
+    int t_high = 0;
+    int t_low =0;
+    do {
+        printf("Enter the high of the day");
+        scanf("%d", &t_high);
+
+
+    } while (valid_entry = 0);
 }
