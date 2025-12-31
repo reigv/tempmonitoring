222470fa46b38590d2f2d1b8295de4b30cb90d8e
reigv <reig.vaeo.soiv099@gmail.com>
2025-12-31 03:52:47 +0000

    1.2

--- tempmonitoring1.c
+++ tempmonitoring1.c
@@ -1,15 +1,18 @@
 #include <stdio.h>
 
 int main() {
-    printf("Temp monitor");
+    printf("Temp monitor\n\n");
 
     int valid_entry = 0; //flag
     int t_high = 0;
     int t_low =0;
     do {
-        printf("Enter the high of the day");
+        printf("Enter the high of the day: ");
         scanf("%d", &t_high);
 
+        printf("Enter the low of the day: ");
+        scanf("%d", &t_low);
+        
 
     } while (valid_entry = 0);
 }
